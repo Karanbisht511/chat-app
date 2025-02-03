7d701d2b813bb953aa9362a160644819d2439b4c
Karan Singh Bisht <karan@Karans-MacBook-Air.local>
2025-02-03 22:32:40 +0530

    fix

--- chat-app/src/stateManagement/api.ts
+++ chat-app/src/stateManagement/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
-const env = import.meta.env.NODE_ENV;
+const env = import.meta.env.MODE;
 export const api = axios.create({
   baseURL:
-    env === "production" ? "/api" : `${import.meta.env.VITE_API_URL}/api`,
+    env === "development" ? `${import.meta.env.VITE_API_URL}/api` : "/",
 });
