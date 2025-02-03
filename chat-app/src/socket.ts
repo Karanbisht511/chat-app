e54f2137588518cfe8d2e6645a7a5288b9ad1dde
Karan Singh Bisht <karan@Karans-MacBook-Air.local>
2025-02-03 22:44:51 +0530

    fix3

--- chat-app/src/socket.ts
+++ chat-app/src/socket.ts
@@ -1,9 +1,10 @@
 import { io } from "socket.io-client";
-
 // "undefined" means the URL will be computed from the `window.location` object
 // const URL = import.meta.env.VITE_API_URL;
 // const URL = import.meta.env.VITE_API_URL2;
-const URL = 'http://localhost:9000';
+const env = import.meta.env.MODE;
+const URL =
+  env === "development" ? `${import.meta.env.VITE_API_URL}/api` : "/api";
 export const socket = io(URL, {
   path: "/api/socketio",
   addTrailingSlash: false,
